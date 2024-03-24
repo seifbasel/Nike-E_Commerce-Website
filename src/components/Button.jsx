665293fb2e0630dd15216df8f9b2eb26b23af5f6
seif basel <seifbasel950@gmail.com>
2024-03-24 02:04:50 +0200

    E_Commerce_Website

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+
+const Button = ({label,iconURL}) => {
+  return (
+    <button className='flex justify-center items-center
+     gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+      bg-coral-red text-white border-coral-red'>
+      {label}
+      <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full h-5 w-5"/>
+    </button>
+  )
+}
+
+export default Button
