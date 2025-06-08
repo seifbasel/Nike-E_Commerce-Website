e8227192dda1c9e639cd73fabfcf73e7c9a32cc4
seif basel <seifbasel950@gmail.com>
2025-06-08 14:28:29 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import {
   Services,
   Subscribe,
   SuperQuality,
-} 
-from "./sections";
+} from "./sections";
 import Nav from "./components/Nav";
-const app=()=>(
+const app = () => (
   <main className="relative">
-    <Nav/>
-    <section className="xl:padding-l wide:padding-r padding-b"><Hero/></section>
-    <section className="padding"><PopularProducts/></section>
-    <section className="padding"><SuperQuality/></section>
-    <section className="padding-x py-10"><Services/></section>
-    <section className="padding-x sm:py-32 py-16 w-full"><Subscribe/></section>
-    <section className="padding bg-black padding-x padding-t pb-8"><Footer/></section>
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="py-10 padding-x">
+      <Services />
+    </section>
+    <section className="w-full py-16 padding-x sm:py-32">
+      <Subscribe />
+    </section>
+    <section className="pb-8 bg-black padding padding-x padding-t">
+      <Footer />
+    </section>
   </main>
 );
 export default app;
